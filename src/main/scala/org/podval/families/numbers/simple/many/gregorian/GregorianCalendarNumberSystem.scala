1deb9e3d7cc3dbd53c8219125dd91d631d2061af
Leonid Dubinsky <dub@podval.org>
2017-08-11 02:11:15 -0400

    numbers.simple.many

--- src/main/scala/org/podval/families/numbers/simple/many/gregorian/GregorianCalendarNumberSystem.scala
+++ src/main/scala/org/podval/families/numbers/simple/many/gregorian/GregorianCalendarNumberSystem.scala
@@ -4,16 +4,15 @@ import org.podval.families.numbers.simple.many.base.NumberSystem.Raw
 import org.podval.families.numbers.simple.many.calendar.CalendarNumberSystem
 
 // TODO can't be final: object extends it.
-class GregorianCalendarNumberSystem[S <: GregorianCalendarNumberSystem[S]]
-  extends CalendarNumberSystem[S]
-{ this: S =>
-  final override type Number = GregorianCalendarNumber[S]
+class GregorianCalendarNumberSystem
+  extends CalendarNumberSystem[GregorianCalendarNumberSystem]
+{
+  final override type Number = GregorianCalendarNumber
 
-  def createNumber(raw: Raw): S#Number = new GregorianCalendarNumber[S](raw) {
-    override def numberSystem: S = GregorianCalendarNumberSystem.this
+  def createNumber(raw: Raw): GregorianCalendarNumberSystem#Number = new GregorianCalendarNumber(raw) {
+    override def numberSystem: GregorianCalendarNumberSystem = GregorianCalendarNumberSystem.this
   }
 }
 
 
-object GregorianCalendarNumberSystem
-  extends GregorianCalendarNumberSystem[GregorianCalendarNumberSystem.type]
+object GregorianCalendarNumberSystem extends GregorianCalendarNumberSystem
